5b1b327598cc66f4d58fca647956aaf6f0b40698
MohonSaha <practicewithmohon@gmail.com>
2023-05-20 13:00:06 +0600

    create api for delete operation

--- index.js
+++ index.js
@@ -100,6 +100,13 @@ async function run() {
         })
 
 
+        // delete toy
+        app.delete('/myToys/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toysCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
 
